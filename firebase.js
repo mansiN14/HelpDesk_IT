d5daa7f4b5801d2285c12d3dde0c04c4792ebaaa
manasi <162135200+mansiN14@users.noreply.github.com>
2025-05-29 12:29:22 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBqvva2cJ6wy8ssrP76Vgh5H9wZNLATWE",
+  authDomain: "ithelpdesk-ebf1e.firebaseapp.com",
+  projectId: "ithelpdesk-ebf1e",
+  storageBucket: "ithelpdesk-ebf1e.firebasestorage.app",
+  messagingSenderId: "163734375056",
+  appId: "1:163734375056:web:38a2e670015e6e73eb2615",
+  measurementId: "G-QYX1WNLZ3C"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
