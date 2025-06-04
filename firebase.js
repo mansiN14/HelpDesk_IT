394a69710c77d0e64b0d44948ce6ca3da747b38d
manasi <162135200+mansiN14@users.noreply.github.com>
2025-06-05 01:30:30 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBBqvva2cJ6wy8ssrP76Vgh5H9wZNLATWE",
   authDomain: "ithelpdesk-ebf1e.firebaseapp.com",
   projectId: "ithelpdesk-ebf1e",
-  storageBucket: "ithelpdesk-ebf1e.firebasestorage.app",
+  storageBucket: "ithelpdesk-ebf1e.appspot.com",
   messagingSenderId: "163734375056",
   appId: "1:163734375056:web:38a2e670015e6e73eb2615",
   measurementId: "G-QYX1WNLZ3C"
@@ -17,5 +18,6 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { app, db, auth };
+export { app, db, auth, storage };
